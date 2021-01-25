05f055b342667c1242f1044ba54904f0127fd0b2
zhuxiuwu <zhuxw@133.cn>
2021-01-25 09:58:07 +0800

    解决获取redis中的值带有b

--- utils/redisset.py
+++ utils/redisset.py
@@ -8,7 +8,7 @@ def redisset():
   port = conf.get('redis', 'port')
   db = conf.get('redis', 'db')
   # 普通连接
-  conn = redis.Redis(host=host, port=int(port),db=db)
+  conn = redis.Redis(host=host, port=int(port),db=db,decode_responses=True) #python3读取redis返回数据带有'b'的问题,在连接数据库的时候加上decode_responses=True即可
   # 可以使用url方式连接到数据库
   # conn = redis.Redis.from_url('redis://127.0.0.1:6379/1')
   conn.set('name', 'LinWOW')
