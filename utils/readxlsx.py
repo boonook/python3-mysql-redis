89be4974020a38f5889aac96faea692e3ffa3c3d
大地瓜 <boonook@163.com>
2021-10-29 09:05:53 +0800

    fix(main): 更新

--- utils/readxlsx.py
+++ utils/readxlsx.py
@@ -12,13 +12,16 @@ def readxlsxfile():
             # print(i)
             # print(item)
             if i>0:
-                dict = {'org':item[0], 'dst':item[1], 'date':item[2],'rdate':item[3],'realprice':item[4],'body':item[6]}
-                datasource.append(dict)
+                item_6 = item[6].strip(',')
+                print(eval(item_6.org))
+                # print(str(i)+str('----')+item_6)
+                # dict = {'org':item[0], 'dst':item[1], 'date':item[2],'rdate':item[3],'realprice':item[4],'body':{'org':item[6].org,'dst':item[6].dst,'orgcn':item[6].orgcn,'dstcn':item[6].dstcn,'date':item[6].date,'rdate':item[6].rdate,'period':item[6].period,'notaxprice':item[6].notaxprice}}
+                # datasource.append(dict)
             else:
                 print(item)
         city_json.append({'name':sheet.name,'data':datasource})               
             # print((sheet._cell_values)[1])
-    data2 = json.dumps(city_json,ensure_ascii=False)
+    data2 = json.dumps(city_json,ensure_ascii=False,separators=(',',':'),indent=2)
     f = open('filename.json','w',encoding='utf-8')
     f.write(data2)
     f.close()  
